615a9a8dee930170e6f8c259edf9f98e889c2a9c
Jayant Verma <iamvermajayant@gmail.com>
2025-02-05 23:05:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout';
+import { ThemeProvider } from './context/theme-provider';
+import Weatherdashboard from './pages/Weatherdashboard';
+import CityPage from './pages/CityPage';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
+function App() {
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme='dark'>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<Weatherdashboard />} />
+              <Route path='/city/:cityname' element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default App
---
+++ src/pages/Weatherdashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Weatherdashboard = () => {
+  return (
+    <div>Weatherdashboard</div>
+  )
+}
+
+export default Weatherdashboard
