d920c56d5e503d5048b322543ad09b169b72a48d
Jayant Verma <iamvermajayant@gmail.com>
2025-02-08 23:24:10 +0530

    added wind speed min and max temp images

--- src/components/CurrentWeatherCard.tsx
+++ src/components/CurrentWeatherCard.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, Droplet, Wind } from "lucide-react";
 import type { GeocodingResponse, WeatherData } from "../api/types"
 import { Card, CardContent } from "./ui/card";
 
@@ -39,22 +39,50 @@ const CurrentWeatherCard = ({ data, locationName }: CurrentWeatherCardProps) => 
                             <p className="text-7xl font-bold tracking-tighter">{formatTemp(temp)}</p>
 
                             <div className="space-y-1">
-                                <p className="text-sm font-medium text-muted-foreground">Feels like {formatTemp(feels_like)}</p> 
-                            <div className="flex gap-2 text-sm font-medium ">
-                                <span className="flex items-center gap-1 text-blue-500">
-                                    <ArrowDown className="h-4 w-4" />
-                                    {formatTemp(temp_min)}
-                                </span>
-                                <span className="flex items-center gap-1 text-blue-500">
-                                    <ArrowUp className="h-4 w-4" />
-                                    {formatTemp(temp_max)}
-                                </span>
-                            </div>
+                                <p className="text-sm font-medium text-muted-foreground">Feels like {formatTemp(feels_like)}</p>
+                                <div className="flex gap-2 text-sm font-medium ">
+                                    <span className="flex items-center gap-1 text-blue-500">
+                                        <ArrowDown className="h-4 w-4" />
+                                        {formatTemp(temp_min)}
+                                    </span>
+                                    <span className="flex items-center gap-1 text-blue-500">
+                                        <ArrowUp className="h-4 w-4" />
+                                        {formatTemp(temp_max)}
+                                    </span>
+                                </div>
                             </div>
 
                         </div>
+
+                        <div className="grid grid-cols-2 gap-2">
+                            <div className="flex items-center gap-2">
+                                <Droplet className="h-4 w-4 text-blue-500" />
+                                <div className="space-y-0.5">
+                                    <p className="text-sm">Humidity</p>
+                                    <p className="text-sm text-muted-foreground">{humidity}%</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <Wind className="h-4 w-4 text-blue-500" />
+                                <div className="space-y-0.5">
+                                    <p className="text-sm">Wind speed</p>
+                                    <p className="text-sm text-muted-foreground">{speed}m/s</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="relative flex items-center justify-center w-full aspect-square max-w-[200px]">
+                            <img src={`http://openweathermap.org/img/wn/${currentWeather.icon}@4x.png`} alt="" />
+                            <div className="absolute bottom-0 text-center">
+                                <p className="text-sm font-medium capitalize">{currentWeather.description}
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+
             </CardContent>
         </Card>
     )
